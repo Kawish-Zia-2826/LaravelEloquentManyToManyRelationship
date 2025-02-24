a7997430fe158503f814c83050215c55b7f22719
Kawish Zia <kawishzia2826@gmail.com>
2025-02-25 01:02:32 +0500

    update

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,16 @@
 <?php
 
+use App\Http\Controllers\RoleController;
+use App\Http\Controllers\UserController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
+
     return view('welcome');
+
+    
 });
+
+Route::resource('users',UserController::class);
+Route::resource('roles',RoleController::class);
+
